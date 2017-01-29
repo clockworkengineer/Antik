d555cae7ab61b20716f99d8ebecd1058debdb07e
clockworkengineer <robert_tizzard@hotmail.com>
2017-01-29 16:55:44 +0000

    CMailIMAP Rev 4.

--- classes/CMailIMAP.cpp
+++ classes/CMailIMAP.cpp
@@ -35,13 +35,42 @@
 // PRIVATE TYPES AND CONSTANTS
 // ===========================
 
-// Line terminator
-
-const std::string CMailIMAP::kEOL("\r\n");
-
 // ==========================
 // PUBLIC TYPES AND CONSTANTS
 // ==========================
+
+//
+// Line terminator
+//
+
+const std::string CMailIMAP::kEOLStr("\r\n");
+
+//
+// Command string constants
+//
+
+const std::string CMailIMAP::kSEARCHStr("SEARCH");
+const std::string CMailIMAP::kSELECTStr("SELECT");
+const std::string CMailIMAP::kEXAMINEStr("EXAMINE");
+const std::string CMailIMAP::kCREATEStr("CREATE");
+const std::string CMailIMAP::kDELETEStr("DELETE");
+const std::string CMailIMAP::kRENAMEStr("RENAME");
+const std::string CMailIMAP::kLOGINStr("LOGIN");
+const std::string CMailIMAP::kSUBSCRIBEStr("SUBSCRIBE");
+const std::string CMailIMAP::kUNSUBSCRIBEStr("UNSUBSCRIBE");
+const std::string CMailIMAP::kLISTStr("LIST");
+const std::string CMailIMAP::kLSUBStr("LSUB");
+const std::string CMailIMAP::kSTATUSStr("STATUS");
+const std::string CMailIMAP::kAPPENDStr("APPEND");
+const std::string CMailIMAP::kCHECKStr("CHECK");
+const std::string CMailIMAP::kCLOSEStr("CLOSE");
+const std::string CMailIMAP::kEXPUNGEStr("EXPUNGE");
+const std::string CMailIMAP::kFETCHStr("FETCH");
+const std::string CMailIMAP::kSTOREStr("STORE");
+const std::string CMailIMAP::kCOPYStr("COPY");
+const std::string CMailIMAP::kNOOPStr("NOOP");
+const std::string CMailIMAP::kLOGOUTStr("LOGOUT");
+const std::string CMailIMAP::kIDLEStr("IDLE");
 
 // ========================
 // PRIVATE STATIC VARIABLES
@@ -101,7 +130,7 @@ void CMailIMAP::waitForCommandResponse(const std::string& commandTag, std::strin
                 commandResponse.copy(this->rxBuffer, len);
                 break;
             }
-            
+
         } else if (this->res != CURLE_AGAIN) {
             std::string errMsg;
             if (std::strlen(this->errMsgBuffer) != 0) {
@@ -111,7 +140,7 @@ void CMailIMAP::waitForCommandResponse(const std::string& commandTag, std::strin
             }
             throw std::runtime_error(std::string("curl_easy_recv() failed: ") + errMsg);
         }
-            
+
     } while (true);
 
 
@@ -124,6 +153,8 @@ void CMailIMAP::waitForCommandResponse(const std::string& commandTag, std::strin
 size_t CMailIMAP::writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
     data->append((char*) ptr, size * nmemb);
     return size * nmemb;
+
+
 }
 
 // ==============
@@ -205,7 +236,10 @@ void CMailIMAP::waitOnIdle(const std::string& imapMailBox) {
 
 void CMailIMAP::connect(void) {
 
+
     if (this->curl) {
+
+        CMailIMAP::BASERESPONSE noopResp;
 
         curl_easy_setopt(this->curl, CURLOPT_PROTOCOLS, CURLPROTO_IMAPS | CURLPROTO_IMAP);
         curl_easy_setopt(this->curl, CURLOPT_USERNAME, this->userName.c_str());
@@ -221,6 +255,8 @@ void CMailIMAP::connect(void) {
         curl_easy_setopt(this->curl, CURLOPT_WRITEFUNCTION, CMailIMAP::writeFunction);
         curl_easy_setopt(this->curl, CURLOPT_WRITEDATA, &this->commandResponse);
         curl_easy_setopt(this->curl, CURLOPT_HEADERDATA, &this->commandHeader);
+
+        noopResp = this->sendCommand(CMailIMAP::kNOOPStr);
 
     }
 
@@ -238,15 +274,15 @@ void CMailIMAP::disconnect() {
 }
 
 //
-// Send single IMAP command and return reply
+// Send single IMAP command and decode/return response.
 //
 
-std::string CMailIMAP::sendIMAPCommand(const std::string& command) {
+CMailIMAP::BASERESPONSE CMailIMAP::sendCommand(const std::string& commandLine) {
 
     this->commandResponse.clear();
     this->commandHeader.clear();
-    
-    curl_easy_setopt(this->curl, CURLOPT_CUSTOMREQUEST, command.c_str());
+
+    curl_easy_setopt(this->curl, CURLOPT_CUSTOMREQUEST, commandLine.c_str());
 
     this->errMsgBuffer[0] = 0;
     this->res = curl_easy_perform(this->curl);
@@ -261,7 +297,320 @@ std::string CMailIMAP::sendIMAPCommand(const std::string& command) {
         throw std::runtime_error(std::string("curl_easy_perform() failed: ") + errMsg);
     }
 
-    return (this->commandHeader);
+    return (CMailIMAP::decodeResponse(commandLine, this->commandHeader));
+
+}
+
+//
+// Extract the contents between to delimeters
+//
+
+std::string CMailIMAP::contentsBetween(const std::string& line, const char first, const char last) {
+    return (line.substr(line.find_first_of(first) + 1, line.find_first_of(last) - line.find_first_of(first) - 1));
+}
+
+//
+// Extract number between two characters (ie. number and spaces)
+//
+
+std::string CMailIMAP::cutOut(const std::string& line, const char seperator) {
+    return (line.substr(line.find_first_of(seperator) + 1, line.find_last_of(seperator) - line.find_first_of(seperator) - 1));
+}
+
+//
+// Decode SELECT/EXAMINE Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeSELECT(const std::string& commandLine, std::istringstream& responseStream) {
+
+    CMailIMAP::SELECTRESPONSE resp(new CMailIMAP::SelectResponse);
+    std::string responseValue;
+
+    resp->command = commandLine.substr(0, commandLine.find_first_of(' '));
+    resp->mailBoxName = commandLine.substr(commandLine.find_last_of(' ') + 1);
+    resp->status = CMailIMAP::RespCode::OK;
+
+    if (resp->mailBoxName.back() == '\"') resp->mailBoxName.pop_back();
+    if (resp->mailBoxName.front() == '\"') resp->mailBoxName = resp->mailBoxName.substr(1);
+
+    for (std::string line; std::getline(responseStream, line, '\n');) {
+
+        line.pop_back();
+
+        if (line.find("* FLAGS") == 0) {
+            responseValue = contentsBetween(line, '(', ')');
+            resp->responseMap.insert({"FLAGS", responseValue});
+
+        } else if (line.find("* OK [PERMANENTFLAGS") == 0) {
+            responseValue = contentsBetween(line, '(', ')');
+            resp->responseMap.insert({"PERMANENTFLAGS", responseValue});
+
+        } else if (line.find("* OK [UIDVALIDITY") == 0) {
+            responseValue = contentsBetween(line, '[', ']');
+            responseValue = responseValue.substr(responseValue.find_first_of(' ') + 1);
+            resp->responseMap.insert({"UIDVALIDITY", responseValue});
+
+        } else if (line.find("* OK [UIDNEXT") == 0) {
+            responseValue = contentsBetween(line, '[', ']');
+            responseValue = responseValue.substr(responseValue.find_first_of(' ') + 1);
+            resp->responseMap.insert({"UIDNEXT", responseValue});
+
+        } else if (line.find("* OK [HIGHESTMODSEQ") == 0) {
+            responseValue = contentsBetween(line, '[', ']');
+            responseValue = responseValue.substr(responseValue.find_first_of(' ') + 1);
+            resp->responseMap.insert({"HIGHESTMODSEQ", responseValue});
+
+        } else if (line.find("* OK [UNSEEN") == 0) {
+            responseValue = contentsBetween(line, '[', ']');
+            responseValue = responseValue.substr(responseValue.find_first_of(' ') + 1);
+            resp->responseMap.insert({"UNSEEN", responseValue});
+
+        } else if (line.find("EXISTS") != std::string::npos) {
+            responseValue = cutOut(line, ' ');
+            resp->responseMap.insert({"EXISTS", responseValue});
+
+        } else if (line.find("RECENT") != std::string::npos) {
+            responseValue = cutOut(line, ' ');
+            resp->responseMap.insert({"RECENT", responseValue});
+
+        } else if (line.find("* CAPABILITY") == 0) {
+            responseValue = line.substr((std::string("* CAPABILITY").length()) + 1);
+            resp->responseMap.insert({"CAPABILITY", responseValue});
+
+        } else if ((line.find("] " + resp->mailBoxName) != std::string::npos) ||
+                (line.find("] " + resp->command + " completed.") != std::string::npos)) {
+            resp->mailBoxAccess = contentsBetween(line, '[', ']');
+
+        } else {
+            std::cerr << "UKNOWN RESPONSE TYPE = [" << line << "]" << std::endl;
+        }
+
+    }
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (resp));
+
+}
+
+//
+// Decode SEARCH Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeSEARCH(const std::string& commandLine, std::istringstream& responseStream) {
+
+    CMailIMAP::SEARCHRESPONSE resp(new CMailIMAP::SearchResponse);
+    uint64_t index;
+
+    resp->command = commandLine.substr(0, commandLine.find_first_of(' '));
+    resp->status = CMailIMAP::RespCode::OK;
+
+    for (std::string line; std::getline(responseStream, line, '\n');) {
+
+        line.pop_back();
+
+        if (line.find("* " + resp->command) == 0) {
+
+            line = line.substr(line.find_first_of(' ') + 1);
+            line = line.substr(line.find_first_of(' ') + 1);
+
+            std::istringstream listStream(line);
+            while (listStream.good()) {
+                listStream >> index;
+                resp->indexes.push_back(index);
+            }
+
+        } else {
+            std::cerr << "UKNOWN RESPONSE TYPE = [" << line << "]" << std::endl;
+        }
+
+
+    }
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (resp));
+
+}
+
+//
+// Decode LIST/LSUB Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeLIST(const std::string& commandLine, std::istringstream& responseStream) {
+
+    CMailIMAP::LISTRESPONSE resp(new CMailIMAP::ListResponse);
+    CMailIMAP::ListRespData mailBoxEntry;
+
+    resp->command = commandLine.substr(0, commandLine.find_first_of(' '));
+    resp->status = CMailIMAP::RespCode::OK;
+
+    for (std::string line; std::getline(responseStream, line, '\n');) {
+
+        line.pop_back();
+
+        if (line.find("* " + resp->command) != std::string::npos) {
+            mailBoxEntry.attributes = contentsBetween(line, '(', ')');
+            mailBoxEntry.hierDel = contentsBetween(line, '\"', '\"').front();
+            if (line.back() != '\"') {
+                mailBoxEntry.name = line.substr(line.find_last_of(' '));
+            } else {
+                line.pop_back();
+                mailBoxEntry.name = line.substr(line.find_last_of('\"'));
+                mailBoxEntry.name += '\"';
+            }
+
+            resp->mailBoxList.push_back(mailBoxEntry);
+
+        } else {
+            std::cerr << "UKNOWN RESPONSE TYPE = [" << line << "]" << std::endl;
+        }
+    }
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (resp));
+
+
+}
+
+//
+// Decode STATUS Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeSTATUS(const std::string& commandLine, std::istringstream& responseStream) {
+
+    CMailIMAP::STATUSRESPONSE resp(new CMailIMAP::StatusResponse);
+    std::string line;
+
+    resp->command = commandLine.substr(0, commandLine.find_first_of(' '));
+    resp->status = CMailIMAP::RespCode::OK;
+
+    for (std::string line; std::getline(responseStream, line, '\n');) {
+
+        line.pop_back();
+
+        if (line.find("* " + resp->command) == 0) {
+
+            line = line.substr(line.find_first_of(' ') + 1);
+            line = line.substr(line.find_first_of(' ') + 1);
+            resp->mailBoxName = line.substr(0, line.find_first_of(' '));
+
+            line = contentsBetween(line, '(', ')');
+
+            std::istringstream listStream(line);
+            std::string item, value;
+
+            while (listStream.good()) {
+                listStream >> item >> value;
+                resp->responseMap.insert({item, value});
+            }
+
+        } else {
+            std::cerr << "UKNOWN RESPONSE TYPE = [" << line << "]" << std::endl;
+        }
+
+    }
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (resp));
+
+}
+
+//
+// Decode EXPUNGE Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeEXPUNGE(const std::string& commandLine, std::istringstream& responseStream) {
+
+    CMailIMAP::EXPUNGERESPONSE resp(new CMailIMAP::ExpungeResponse);
+    std::string line;
+
+    resp->command = commandLine.substr(0, commandLine.find_first_of(' '));
+    resp->status = CMailIMAP::RespCode::OK;
+
+    for (std::string line; std::getline(responseStream, line, '\n');) {
+
+        line.pop_back();
+
+        if (line.find("EXISTS") != std::string::npos) {
+            line = cutOut(line, ' ');
+            resp->exists.push_back(std::strtoull(line.c_str(), nullptr, 10));
+        } else if (line.find("EXPUNGE") != std::string::npos) {
+            line = cutOut(line, ' ');
+            resp->expunged.push_back(std::strtoull(line.c_str(), nullptr, 10));
+        } else {
+            std::cerr << "UKNOWN RESPONSE TYPE = [" << line << "]" << std::endl;
+        }
+
+    }
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (resp));
+
+
+}
+
+//
+// Decode STORE Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeSTORE(const std::string& commandLine, std::istringstream& responseStream) {
+
+    CMailIMAP::STORERESPONSE resp(new CMailIMAP::StoreResponse);
+    StoreRespData storeData;
+    std::string line;
+
+    resp->command = commandLine.substr(0, commandLine.find_first_of(' '));
+    resp->status = CMailIMAP::RespCode::OK;
+
+    for (std::string line; std::getline(responseStream, line, '\n');) {
+
+        line.pop_back();
+
+        if (line.find("FETCH") != std::string::npos) {
+            storeData.flags = contentsBetween(line.substr(line.find_first_of('(') + 1), '(', ')');
+            storeData.index = std::strtoull(cutOut(line, ' ').c_str(), nullptr, 10);
+            resp->storeList.push_back(storeData);
+        } else {
+            std::cerr << "UKNOWN RESPONSE TYPE = [" << line << "]" << std::endl;
+        }
+
+    }
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (resp));
+
+
+}
+
+//
+// Decode Command Response
+//
+
+CMailIMAP::BASERESPONSE CMailIMAP::decodeResponse(const std::string& commandLine, const std::string & commandResponse) {
+
+    std::istringstream responseStream(commandResponse);
+    std::istringstream commandStream(commandLine);
+    std::string command = commandLine.substr(0, commandLine.find_first_of(' '));
+
+    if ((command.compare(CMailIMAP::kLISTStr) == 0) || (command.compare(CMailIMAP::kLSUBStr) == 0)) {
+        return (CMailIMAP::decodeLIST(commandLine, responseStream));
+    }
+
+    if (command.compare(CMailIMAP::kSEARCHStr) == 0) {
+        return (CMailIMAP::decodeSEARCH(commandLine, responseStream));
+    }
+
+    if ((command.compare(CMailIMAP::kSELECTStr) == 0) || (commandLine.compare(CMailIMAP::kEXAMINEStr) == 0)) {
+        return (CMailIMAP::decodeSELECT(commandLine, responseStream));
+    }
+
+    if (command.compare(CMailIMAP::kSTATUSStr) == 0) {
+        return (CMailIMAP::decodeSTATUS(commandLine, responseStream));
+    }
+
+    if (command.compare(CMailIMAP::kEXPUNGEStr) == 0) {
+        return (CMailIMAP::decodeEXPUNGE(commandLine, responseStream));
+    }
+
+    if (command.compare(CMailIMAP::kSTOREStr) == 0) {
+        return (CMailIMAP::decodeSTORE(commandLine, responseStream));
+    }
+
+
+    return (static_cast<CMailIMAP::BASERESPONSE> (new CMailIMAP::BResponse{command, RespCode::OK}));
 
 }
 
@@ -284,7 +633,7 @@ CMailIMAP::~CMailIMAP() {
     if (this->curl) {
         curl_easy_cleanup(this->curl);
     }
-    
+
 }
 
 //
--- classes/CMailIMAP.hpp
+++ classes/CMailIMAP.hpp
@@ -20,7 +20,10 @@
 #include <cstring>
 #include <string>
 #include <vector>
+#include <memory>
 #include <stdexcept>
+#include <unordered_map>
+#include <sstream>
 
 //
 // libcurl definitions
@@ -33,110 +36,277 @@
 // ================
 
 class CMailIMAP {
-    
 public:
-    
+
     // ==========================
     // PUBLIC TYPES AND CONSTANTS
     // ==========================
+
+    //
+    // LIST response data
+    //
+    
+    struct ListRespData {
+        uint8_t     hierDel;       // Hierarchy Delimeter
+        std::string attributes;    // Mailbox attributes
+        std::string name;          // Mailbox name
+    };
+    
+    //
+    // STORE responses data
+    //
+    
+    struct StoreRespData {
+        uint64_t index;      // EMail Index/UID
+        std::string flags;   // EMail flags
+    };
+
+    //
+    // Enumeration of command codes (not used at present)
+    //
+
+    enum Commands {
+        STARTTLS = 0,   // Supported
+        AUTHENTICATE,   // Supported
+        LOGIN,          // Supported
+        SELECT,         // Supported
+        EXAMINE,        // Supported
+        CREATE,         // Supported
+        DELETE,         // Supported
+        RENAME,         // Supported
+        SUBSCRIBE,      // Supported
+        UNSUBSCRIBE,    // Supported
+        LIST,           // Supported
+        LSUB,           // Supported
+        STATUS,         // Supported
+        APPEND,         // <-- Special handling (come back)
+        CHECK,          // Supported
+        CLOSE,          // Supported
+        EXPUNGE,        // Supported
+        SEARCH,         // Supported
+        FETCH,          // <-- Special handling (come back)
+        STORE,          // Supported
+        COPY,           // Supported
+        UID,            // <-- Special handling (come back)
+        NOOP,           // <-- Special handling (come back)
+        LOGOUT,         // <-- Special handling (come back)
+        IDLE            // Supported (waitOnIdle)
+    };
+
+    //
+    // Command response code enumeration (curl_easy_perform() disconnects on
+    // any error so all codes returned are OK (need to address).
+    //
+    
+    enum class RespCode {
+        OK = 0,
+        NO,
+        BAD
+    };
+
+    //
+    // Command string constants
+    //
+    
+    static const std::string kSEARCHStr;
+    static const std::string kSELECTStr;
+    static const std::string kEXAMINEStr;
+    static const std::string kCREATEStr;
+    static const std::string kDELETEStr;
+    static const std::string kRENAMEStr;
+    static const std::string kLOGINStr;
+    static const std::string kSUBSCRIBEStr;
+    static const std::string kUNSUBSCRIBEStr;
+    static const std::string kLISTStr;
+    static const std::string kLSUBStr;
+    static const std::string kSTATUSStr;
+    static const std::string kAPPENDStr;
+    static const std::string kCHECKStr;
+    static const std::string kCLOSEStr;
+    static const std::string kEXPUNGEStr;
+    static const std::string kFETCHStr;
+    static const std::string kSTOREStr;
+    static const std::string kCOPYStr;
+    static const std::string kNOOPStr;
+    static const std::string kLOGOUTStr;
+    static const std::string kIDLEStr;
+
+    //
+    // Decoded command structures.
+    // 
+    
+    struct BResponse {
+        std::string command;
+        CMailIMAP::RespCode status;
+    };
+
+    struct SearchResponse : public BResponse {
+        std::vector<uint64_t> indexes;
+    };
+
+    struct SelectResponse : public BResponse {
+        std::string mailBoxName;
+        std::string mailBoxAccess;
+        std::unordered_map<std::string, std::string> responseMap;
+    };
+
+    struct ExamineResponse : public SelectResponse {
+    };
+
+    struct ListResponse : public BResponse {
+        std::vector<CMailIMAP::ListRespData> mailBoxList;
+    };
+    
+    struct LSubResponse : public ListResponse {
+    };
+    
+    struct StatusResponse : public BResponse {
+        std::string mailBoxName;
+        std::unordered_map<std::string, std::string> responseMap;
+    };
+    
+    struct ExpungeResponse : public BResponse {
+        std::vector<uint64_t> exists;
+        std::vector<uint64_t> expunged;
+    };
+   
+    struct StoreResponse : public BResponse {
+        std::vector<StoreRespData> storeList;
+    };
+    
+    //
+    // Command response structure shared pointer wrapper.
+    //
+    
+    typedef  std::shared_ptr<CMailIMAP::BResponse> BASERESPONSE; 
+    typedef  std::shared_ptr<CMailIMAP::SearchResponse> SEARCHRESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::SelectResponse> SELECTRESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::ExamineResponse> EXAMINERESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::ListResponse> LISTRESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::LSubResponse> LSUBRESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::StatusResponse> STATUSRESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::ExpungeResponse> EXPUNGERESPONSE;
+    typedef  std::shared_ptr<CMailIMAP::StoreResponse> STORERESPONSE;
+    
     
     // ============
     // CONSTRUCTORS
     // ============
-    
+
     //
     // Main constructor
     //
-    
+
     CMailIMAP();
-    
+
     // ==========
     // DESTRUCTOR
     // ==========
-    
+
     virtual ~CMailIMAP();
 
     // ==============
     // PUBLIC METHODS
     // ==============
-    
+
     //
     // Set email server account details
     //
-    
+
     void setServer(const std::string& serverURL);
     void setUserAndPassword(const std::string& userName, const std::string& userPassword);
-    
+
     //
     // Wait on IDLE for mailbox
     //
-    
+
     void waitOnIdle(const std::string& imapMailBox);
-    
+
     //
     // IMAP connect, send command and disconnect
     //
-    
+
     void connect(void);
-    std::string  sendIMAPCommand(const std::string& command);
+    std::shared_ptr<CMailIMAP::BResponse> sendCommand(const std::string& commandLine);
     void disconnect(void);
-       
+
     // IMAP initialization and closedown processing
-    
+
     static void init();
     static void closedown();
-    
+
+
     // ================
     // PUBLIC VARIABLES
     // ================
-    
+
 private:
 
     // ===========================
     // PRIVATE TYPES AND CONSTANTS
     // ===========================
-  
-    static const std::string kEOL;                  // End of line
-    
+
+    static const std::string kEOLStr; // End of line
+
     // =====================
     // DISABLED CONSTRUCTORS
     // =====================
- 
+
     // ===============
     // PRIVATE METHODS
     // ===============
-    
+
     //
     // IDLE command functions. Talks to server using curl_easy_send/recv() as this is the only
     // way to get IDLE to work.
     //
-    
+
     void sendCommandDirect(const std::string& command);
     void waitForCommandResponse(const std::string& commandTag, std::string& commandResponse);
-    
+
     //
     // Add libcurl receive data to reply string.
     //
-    
+
     static size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data);
+  
+    //
+    // Command response decode utility methods
+    //
     
+    static std::string contentsBetween(const std::string& line, const char first, const char last);
+    static std::string cutOut(const std::string& line, const char seperator);
+    
+    //
+    // Command response decode methods
+    //
+    
+    static CMailIMAP::BASERESPONSE decodeLIST(const std::string& commandLine, std::istringstream& responseStream);
+    static CMailIMAP::BASERESPONSE decodeSEARCH(const std::string& commandLine, std::istringstream& responseStream);
+    static CMailIMAP::BASERESPONSE decodeSELECT(const std::string& commandLine, std::istringstream& responseStream);
+    static CMailIMAP::BASERESPONSE decodeSTATUS(const std::string& commandLine, std::istringstream& responseStream);
+    static CMailIMAP::BASERESPONSE decodeEXPUNGE(const std::string& commandLine, std::istringstream& responseStream);
+    static CMailIMAP::BASERESPONSE decodeSTORE(const std::string& commandLine, std::istringstream& responseStream);
+    
+    static CMailIMAP::BASERESPONSE decodeResponse(const std::string& commandLine, const std::string& commandResponse);
+
     // =================
     // PRIVATE VARIABLES
     // =================
-    
-    std::string userName="";                  // Email account user name
-    std::string userPassword="";              // Email account user name password
-    std::string serverURL="";                 // IMAP server URL
-    
-    CURL     *curl=nullptr;                   // curl handle
-    CURLcode res = CURLE_OK;                  // curl status
-  
-    std::string commandResponse;              // IMAP command response
-    std::string commandHeader;                // IMAP reply header
- 
-    char rxBuffer[CURL_MAX_WRITE_SIZE];       // IMAP rx buffer
-    char errMsgBuffer[CURL_ERROR_SIZE];       // IMAP error string buffer
-    
+
+    std::string userName = ""; // Email account user name
+    std::string userPassword = ""; // Email account user name password
+    std::string serverURL = ""; // IMAP server URL
+
+    CURL *curl = nullptr; // curl handle
+    CURLcode res = CURLE_OK; // curl status
+
+    std::string commandResponse; // IMAP command response
+    std::string commandHeader;   // IMAP reply header
+
+    char rxBuffer[CURL_MAX_WRITE_SIZE]; // IMAP rx buffer
+    char errMsgBuffer[CURL_ERROR_SIZE]; // IMAP error string buffer
+
 };
 
 #endif /* CMAILIMAP_HPP */
