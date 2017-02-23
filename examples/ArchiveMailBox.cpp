f8ac2b2066a22fefa78acda78c455b3edff34bfd
clockworkengineer <robert_tizzard@hotmail.com>
2017-02-23 14:38:35 +0000

    Addition of --updates option.

--- examples/ArchiveMailBox.cpp
+++ examples/ArchiveMailBox.cpp
@@ -14,9 +14,11 @@
 // Program: ArchiveMailBox
 //
 // Description: Log on to a given IMAP server and download all emails for a specified
-// mailbox and create an .eml file for them  in a specified destination folder. Note: 
-// The .eml files are created within a sub-folder with the mailbox name and with 
-// filenames consisting of the mail UID prefix and then subject line.
+// mailbox and create an .eml file for them  in a specified destination folder. The
+// .eml files are created within a sub-folder with the mailbox name and with filenames 
+// consisting of the mail UID prefix and then subject line. If parameter --updates is set 
+// then the date of the newest .eml in the destination folder is used as the basis of
+// the IMAP search (ie. only download new e-mails).
 //
 // Note: At present MIME encoded-words in the subject are not decoded and can result in
 // an weird file name. At a later date it is intended to deal with this correctly.
@@ -47,6 +49,7 @@
 
 #include "boost/program_options.hpp" 
 #include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
@@ -56,12 +59,13 @@ namespace fs = boost::filesystem;
 //
 
 struct ParamArgData {
-    std::string userNameStr;            // Email account user name
-    std::string userPasswordStr;        // Email account user name password
-    std::string serverURLStr;           // SMTP server URL
-    std::string mailBoxNameStr;         // Mailbox name
-    fs::path    destinationFolder;      // Destination folder for attachments
-    std::string configFileNameStr;      // Configuration file name
+    std::string userNameStr; // Email account user name
+    std::string userPasswordStr; // Email account user name password
+    std::string serverURLStr; // SMTP server URL
+    std::string mailBoxNameStr; // Mailbox name
+    fs::path destinationFolder; // Destination folder for attachments
+    std::string configFileNameStr; // Configuration file name
+    bool bOnlyUpdates; // =true search date since last .eml archived
 };
 
 //
@@ -69,6 +73,12 @@ struct ParamArgData {
 //
 
 const int kMaxSubjectLine = 80;
+
+//
+// .eml file exstention
+//
+
+const std::string kEMLFileExt(".eml");
 
 //
 // Exit with error message/status
@@ -95,7 +105,8 @@ void addCommonOptions(po::options_description& commonOptions, ParamArgData& argD
             ("user,u", po::value<std::string>(&argData.userNameStr)->required(), "Account username")
             ("password,p", po::value<std::string>(&argData.userPasswordStr)->required(), "User password")
             ("mailbox,m", po::value<std::string>(&argData.mailBoxNameStr)->required(), "Mailbox name")
-            ("destination,d", po::value<fs::path>(&argData.destinationFolder)->required(), "Destination for attachments");
+            ("destination,d", po::value<fs::path>(&argData.destinationFolder)->required(), "Destination for attachments")
+            ("updates,u", "Search since last file archived.");
 
 }
 //
@@ -103,6 +114,10 @@ void addCommonOptions(po::options_description& commonOptions, ParamArgData& argD
 //
 
 void procCmdLine(int argc, char** argv, ParamArgData &argData) {
+
+    // Default values
+
+    argData.bOnlyUpdates = false;
 
     // Define and parse the program options
 
@@ -143,6 +158,10 @@ void procCmdLine(int argc, char** argv, ParamArgData &argData) {
             }
         }
 
+        if (vm.count("updates")) {
+            argData.bOnlyUpdates = true;
+        }
+
         po::notify(vm);
 
     } catch (po::error& e) {
@@ -177,20 +196,20 @@ void fetchEmailAndArchive(CMailIMAP& imap, fs::path& destinationFolder, std::uin
             } else if (resp.first.find("BODY[HEADER.FIELDS (SUBJECT)]") == 0) {
                 // Modify subject line removing prefix, any non printable and trailing spaces.
                 // Also limit size as this will cause issues with filename lengths if excessively
-                // long.
-                subject = resp.second.substr(9); 
+                // long. Also remove possible hierarchy delimeters.
+                subject = resp.second.substr(9);
                 for (auto &ch : subject) {
-                    if (!std::isprint(ch)) {
+                    if (!std::isprint(ch) || (ch == '/') || (ch == '\\')) {
                         ch = ' ';
                     }
                 }
-                while(!subject.empty() && (subject.back()==' ')) {
+                while (!subject.empty() && (subject.back() == ' ')) {
                     subject.pop_back();
                 }
-                if (subject.length()>kMaxSubjectLine) {
+                if (subject.length() > kMaxSubjectLine) {
                     subject = subject.substr(0, kMaxSubjectLine);
                 }
-             } 
+            }
         }
     }
 
@@ -198,7 +217,7 @@ void fetchEmailAndArchive(CMailIMAP& imap, fs::path& destinationFolder, std::uin
 
     if (!emailBody.empty()) {
         fs::path fullFilePath = destinationFolder;
-        fullFilePath /= "(" + std::to_string(index) + ") " + subject + ".eml";
+        fullFilePath /= "(" + std::to_string(index) + ") " + subject + kEMLFileExt;
         if (!fs::exists(fullFilePath)) {
             std::istringstream emailBodyStream(emailBody);
             std::ofstream ofs(fullFilePath.string(), std::ios::binary);
@@ -209,6 +228,49 @@ void fetchEmailAndArchive(CMailIMAP& imap, fs::path& destinationFolder, std::uin
             }
         }
     }
+
+}
+
+//
+// Find the date on the last modified .eml file and use for SEARCH
+//
+
+std::string getSearchDate(fs::path destinationFolder) {
+
+    std::string dateBuffer(32, ' ');
+                
+    if (fs::exists(destinationFolder) && fs::is_directory(destinationFolder)) {
+
+        std::time_t lastModified;
+        std::vector<std::time_t> fileTimes;
+
+        for (auto& entry : boost::make_iterator_range(fs::directory_iterator(destinationFolder),{})) {
+            if (fs::is_regular_file(entry.status()) && (entry.path().extension().compare(kEMLFileExt) == 0)) {
+                fileTimes.push_back(fs::last_write_time(entry.path()));
+            }
+        }
+
+        if (!fileTimes.empty()) {
+            lastModified = fileTimes.back();
+            fileTimes.pop_back();
+            while (!fileTimes.empty()) {
+                if (std::difftime(lastModified, fileTimes.back()) < 0) {
+                    lastModified = fileTimes.back();
+                }
+                fileTimes.pop_back();
+            }
+
+            std::tm * ptm = std::localtime(&lastModified);
+            dateBuffer.resize(std::strftime(&dateBuffer[0], dateBuffer.length(), "%d-%b-%Y", ptm));
+
+        } else {
+            dateBuffer="";
+        }
+
+
+    }
+
+    return (dateBuffer);
 
 }
 
@@ -224,6 +286,7 @@ int main(int argc, char** argv) {
         CMailIMAP imap;
         std::string parsedResponseStr;
         CMailIMAPParse::BASERESPONSE parsedResponse;
+        std::string searchDate;
 
         // Read in command line parameters and process
 
@@ -237,12 +300,19 @@ int main(int argc, char** argv) {
 
         imap.setServer(argData.serverURLStr);
         imap.setUserAndPassword(argData.userNameStr, argData.userPasswordStr);
-        
+
         // Create destination folder
 
         argData.destinationFolder /= argData.mailBoxNameStr;
         if (!argData.destinationFolder.string().empty() && !fs::exists(argData.destinationFolder)) {
             fs::create_directory(argData.destinationFolder);
+        }
+
+
+        // Get newest file creation date for search
+
+        if (argData.bOnlyUpdates) {
+            searchDate = getSearchDate(argData.destinationFolder);
         }
 
         // Connect
@@ -259,11 +329,17 @@ int main(int argc, char** argv) {
 
         // SEARCH for all present email and then create an archive for them.
 
-        parsedResponseStr = imap.sendCommand("UID SEARCH 1:*");
+        if (!searchDate.empty()) {
+            std::cout << "Searching from [" << searchDate << "]" << std::endl;
+            parsedResponseStr = imap.sendCommand("UID SEARCH SENTSINCE " + searchDate);
+        } else {
+            parsedResponseStr = imap.sendCommand("UID SEARCH 1:*");
+        }
+
         parsedResponse = CMailIMAPParse::parseResponse(parsedResponseStr);
         if (parsedResponse->status != CMailIMAPParse::RespCode::OK) {
             throw CMailIMAP::Exception("IMAP SEARCH " + parsedResponse->errorMessageStr);
-            
+
         } else {
             auto *ptr = static_cast<CMailIMAPParse::SearchResponse *> (parsedResponse.get());
             std::cout << "Messages found = " << ptr->indexes.size() << std::endl;
@@ -273,10 +349,10 @@ int main(int argc, char** argv) {
         }
 
         imap.disconnect();
-        
-    //
-    // Catch any errors
-    //    
+
+        //
+        // Catch any errors
+        //    
 
     } catch (CMailIMAP::Exception &e) {
         exitWithError(e.what());
